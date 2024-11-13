5580d7300cf0ea9d91e8b92145a96c23871052bb
sunhurian <sunhurian@tango.me>
2024-11-13 21:27:44 +0200

    init

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Page() {
+  console.log("Customers page");
+
+  return <div>Customers</div>;
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+export default async function Page() {
+  console.log("Invoices page");
+
+  //   await new Promise((resolve, reject) => setTimeout(reject, 1000));
+  return <div>Invoices</div>;
+}
